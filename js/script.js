2dc1b5fe12385b562014ef2b3803b060b5781d5b
Егор <saleroses@yandex.ru>
2023-01-26 15:33:48 +0300

    +1

--- js/script.js
+++ js/script.js
@@ -11,4 +11,22 @@ function makeUpperCase(str) {
     return str.toUpperCase();
   }
 
-  // CW в
+  // CW возраст
+  function getAge(inputString){
+    return parseInt(inputString);
+  }
+
+  // Из книги
+  let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+
+  let output;
+
+  function tabl () {
+    for (let i = 0; i < scores.length; i++) {
+        output = "Babble solution #" + i + " score: " + scores[i];
+        console.log(output);
+    }
+  }
+
+  tabl ();
+
