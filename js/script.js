7045ea926bf4e648daa14e77e41d09316d480e40
Егор <saleroses@yandex.ru>
2023-01-26 16:00:53 +0300

    +1

--- js/script.js
+++ js/script.js
@@ -16,17 +16,30 @@ function makeUpperCase(str) {
     return parseInt(inputString);
   }
 
-  // Из книги
+  // Из книги -------------------------------
   let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
   let output;
 
+  let highScore = 0;
+
   function tabl () {
-    for (let i = 0; i < scores.length; i++) {
-        output = "Babble solution #" + i + " score: " + scores[i];
-        console.log(output);
+        for (let i = 0; i < scores.length; i++) {
+            output = "Babble solution #" + i + " score: " + scores[i];
+            console.log(output);
+            if (scores[i] > highScore) {
+                highScore = scores[i];
+            }
+        }
+    console.log("Bubbles test: " + scores.length);
+    console.log("Highest babble score: " + highScore);
     }
-  }
 
-  tabl ();
 
+
+tabl ();
+
+
+
+
+ // Из книги -------------------------------
