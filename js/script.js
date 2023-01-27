c73b94e2d96b7e518a6ab25b3778763c69be27de
Егор <saleroses@yandex.ru>
2023-01-27 19:19:59 +0300

    +1

--- js/script.js
+++ js/script.js
@@ -20,21 +20,30 @@ function makeUpperCase(str) {
   let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
   let output;
-
+ 
   let highScore = 0;
 
   function tabl () {
         for (let i = 0; i < scores.length; i++) {
-            output = "Babble solution #" + i + " score: " + scores[i];
+            output = "Мыльный раствор #" + i + " баллы: " + scores[i];
             console.log(output);
             if (scores[i] > highScore) {
                 highScore = scores[i];
             }
         }
-    console.log("Bubbles test: " + scores.length);
-    console.log("Highest babble score: " + highScore);
+        console.log("Растворов всего: " + scores.length);
+        console.log("Лучний тестируемый, баллов: " + highScore);
+
+        let bestSolution = [];
+        for (let i = 0; i < scores.length; i++)
+        if (scores[i] == highScore) {
+            bestSolution.push(i);
+        }
+        console.log("Лучший балл набрали образцы: " + bestSolution);
     }
 
+    
+   tabl ();
 // Из книги -------------------------------
 
 
@@ -47,5 +56,19 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
       return false;
     }
   };
-  
   // CW расчет топлива
+
+  // Sum Numbers
+function sum (numbers) {
+    "use strict";
+  return numbers.reduce((a, b) => a + b, 0);
+};
+  // Sum Numbers
+
+  // Создание пустого массива
+  let massive = [];
+  massive.push("number0");
+  massive.push("number2");
+
+  // Создание пустого массива
+
