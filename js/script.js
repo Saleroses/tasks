042c60d81aebacccf4cb236564f1641965d2c476
Егор <saleroses@yandex.ru>
2023-01-27 20:49:43 +0300

    +1

--- js/script.js
+++ js/script.js
@@ -19,6 +19,8 @@ function makeUpperCase(str) {
   // Из книги -------------------------------
   let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
+  let costs = [25, 27, 25, 25, 25, 25, 33, 31, 25, 29, 27, 22, 31, 25, 25, 33, 21, 25, 25, 25, 28, 25, 24, 22, 20, 25, 30, 25, 24, 25, 25, 25, 27, 25, 26, 29];
+
 function tabl () {
     let output;
     let highScore = 0;
@@ -41,11 +43,29 @@ function best (scores, highScore) {
         bestSolution.push(i);
     }
     console.log("Лучший балл набрали образцы: " + bestSolution);
-    return bestSolution;
-    
+    return bestSolution;  
 }
- let highScore = tabl();
- let bestSolution = best (scores, highScore)
+
+let highScore = tabl();
+let bestSolution = best (scores, highScore)
+
+function effective (scores, costs, highScore) {
+    let cost = 100;
+    let index;
+    for (let i = 0; i < scores.length; i++)
+    if (scores[i] == highScore) {
+        if (cost > costs[i]) {
+            index = i;
+            cost = costs[i];
+        }
+    }
+    console.log("best " + index);
+    return index;
+}
+effective (scores, costs, highScore);
+
+
+
 
    
 // Из книги -------------------------------
