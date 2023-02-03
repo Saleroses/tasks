1c701094317536ca91f829466406e938ffee91c8
Егор <saleroses@yandex.ru>
2023-02-03 23:31:39 +0300

    +1

--- js/script.js
+++ js/script.js
@@ -404,3 +404,27 @@ function golfScore(par, strokes) {
     else if(strokes >= par + 3) return "Go Home!";
 }
 //////
+
+////
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+  case 1:
+    answer = "alpha";
+    break;
+  case 2:
+    answer = "beta";
+    break;
+  case 3:
+    answer = "gamma";
+    break;
+  case 4:
+    answer = "delta";
+    break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+//////
